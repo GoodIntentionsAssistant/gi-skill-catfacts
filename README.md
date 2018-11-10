83b5a221d0eccc09f18cf8ac04c8a869a0dda2d1
Darren Moore <darren.m@firecreekweb.com>
2018-11-10 14:35:06 +0700

    Init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Good Intentions Skill Cat Facts
+
+Cat facts skill for Good Intentions Assistant
+
+
+## Documentation
+
+http://goodintentions.firecreekweb.com
---
+++ cat_facts_skill.js
@@ -0,0 +1,19 @@
+/**
+ * Cat Facts Skill
+ */
+var Skill = require('../../../src/Skill/skill');
+
+module.exports = class CatFactsSkill extends Skill {
+
+/**
+ * Constructor
+ *
+ * @param App app
+ * @access public
+ * @return void
+ */
+	constructor(app) {
+		super(app);
+	}
+
+}
